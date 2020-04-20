8ab701e37932a94a09b1523e08d66cc13fb584f0
Victoria Renee Mowbray <vicki29644@icloud.com>
2020-04-20 23:18:36 +0000

    Automatically backed up by Learn

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -1,4 +1,9 @@
-def plus_two(num)
-	num + 2
-	num
+def plus_two(num)[1] pry (# <RSpec:: ExampleGroups::Plus Two>)> num 
+	sum = num + 2 => 3
+	num + [2] pry (# <RSpec:: ExampleGroups::Plus Two>)> num 
+  binding.pry
 end
+
+
+
+[1] pry (# <RSpec:: ExampleGroups::Plus Two>)> num 
--- lib/pry_is_awesome.rb
+++ lib/pry_is_awesome.rb
@@ -1,12 +1,12 @@
 require 'pry'
-
-def prying_into_the_method
-	inside_the_method = "We're inside the method"
-	puts inside_the_method
-	puts "We're about to stop because of pry!"
-	binding.pry
-	this_variable_hasnt_been_interpreted_yet = "The program froze before it could read me!" 
-	puts this_variable_hasnt_been_interpreted_yet
+def prying_into_the_method(x,y)
+  inside_the_method = "We're inside the method"
+  puts inside_the_method                    
+  puts "we're about to stop because of pry!" 
+  this_variable_hasnt_been_interpreted_yet =
+  "The program froze before it could read me!" 
+  puts this_variable_hasnt_been_interpreted_yet
 end
+ [1] prying_into_the_method(3,2)
 
-prying_into_the_method
+
