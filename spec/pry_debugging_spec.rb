26e87793f893b4e9b92762feaa092d60e552cc28
Victoria Renee Mowbray <vicki29644@icloud.com>
2020-04-21 00:24:19 +0000

    Automatically backed up by Learn

--- spec/pry_debugging_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'pry'
-require './lib/pry_debugging.rb'
-describe "#plus_2" do 
-it "takes in a number as an argument and returns the sum of that number and 2" do
-expect(plus_two(3) + (5)
-sum = 5
-end
-
